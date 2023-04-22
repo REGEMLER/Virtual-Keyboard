cf014e9c7f88a9846274eadf75ec8d42b4d4618d
YarNight <yaroslav239@gmail.com>
2023-04-22 11:21:45 +0300

    feat: add markup and styles of visual keyboard

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const textarea = document.getElementById("textarea"); 
+const keyboard = document.getElementById("keyboard"); 
+
+function handler(event){
+    console.log(event.code);
+    console.log(event.key);
+}
+
+function clickKeyboard(event){
+    if(event.target.classList.contains("e-key")){
+        console.log(`This is key ${event.target.id}`)
+    } else if(event.target.classList.contains("e-code")){
+        console.log(`This is code ${event.target.id}`)
+    } 
+}
+
+// textarea.addEventListener("keydown", handler);
+keyboard.addEventListener("click", clickKeyboard);
+document.body.addEventListener("keydown", handler);
